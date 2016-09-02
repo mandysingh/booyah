2774994a00cfa6bab2a8851658e05d75f6292ba8
Neeraj Sharma <neeraj.sharma@proptiger.com>
2016-09-02 14:23:10 +0530

    added area

--- test.py
+++ test.py
@@ -42,6 +42,10 @@ def find_bhk(text):
     rs = re.search('(\d+)[\s]{,3}([bB]edrooms?|bhk|BHK|Bhk|rk|RK|Rk)', text)
     return rs.group(1) if rs else ""
 
+def find_area(text):
+    rs = re.search('(\d+)[\s]{,3}(sq\.?[\s]{,3}ft\.?s?)',text)
+    return rs.group(1) if rs else ""
+
 
 def ie_preprocess(document):
     sentences = nltk.sent_tokenize(document)
@@ -143,6 +147,7 @@ def make_req():
     payload["rows"] = 200
     payload["fl"] = ["LISTING_DESCRIPTION","LISTING_ID"]
     payload["fl"] += ["LISTING_LATITUDE","LISTING_LONGITUDE"]
+    payload["fl"] += ["BEDROOMS", "SIZE"]
     host = "http://localhost:8983/solr/collection_mp/select"
     #q=*:*&fq=DOCUMENT_TYPE:DIRTY_LISTING&fq=LISTING_POSTED_DATE:[2016-08-01T00:00:00Z TO *]&fq=LISTING_STATUS:Active&fq=UNIT_TYPE:Apartment&fq=LISTING_CATEGORY:Primary
     #&sort=LISTING_QUALITY_SCORE desc, LISTING_SELLER_COMPANY_SCORE desc&rows=2000&fl=LISTING_DESCRIPTION&wt=json
@@ -157,16 +162,30 @@ def make_req():
     metrics["nearby_passed_listings"] = []
     metrics["nearby_failed_listings"] = []
 
+    metrics["bhk_total"] = 0
+    metrics["bhk_passed"] = 0
+    metrics["bhk_passed_listings"] = []
+    metrics["bhk_failed_listings"] = []
+
+    metrics["size_total"] = 0
+    metrics["size_passed"] = 0
+    metrics["size_passed_listings"] = []
+    metrics["size_failed_listings"] = []
+
     nnn = []
+    area = []
     for doc in docs:
         desc = doc["LISTING_DESCRIPTION"]
         lat = doc.get("LISTING_LATITUDE", 0.0)
         lng = doc.get("LISTING_LONGITUDE", 0.0)
         listing_id = doc["LISTING_ID"]
+        bedroom = str(doc["BEDROOMS"])
+        size = doc["SIZE"]
+
         print desc
         print find_bhk(desc)
         print doc["LISTING_ID"], doc.get("LISTING_LATITUDE", 0.0), doc.get("LISTING_LONGITUDE", 0.0)
-        print_results(desc)
+        #print_results(desc)
         print ""
 
         ### start processing
@@ -179,7 +198,7 @@ def make_req():
             metrics["nearby_total"] += 1
             val = True
             for n in nearby:
-                if  n in nearby_safe_list or any(regex.match(n.lower()) for regex in nearby_safe_regex) or match_with_google(lat, lng, n, 25):
+                if  n in nearby_safe_list or any(regex.match(n.lower()) for regex in nearby_safe_regex) :#or match_with_google(lat, lng, n, 25):
                     time.sleep(1)  
                 else:
                     val = False
@@ -191,9 +210,38 @@ def make_req():
             else:
                 metrics["nearby_failed_listings"].append(listing_id)
         project = chunk_project(sentences)
+
+        ## bhk check
+        bhk_found = find_bhk(desc)
+        if bhk_found:
+            metrics["bhk_total"] += 1
+
+            if bhk_found == bedroom:
+                metrics["bhk_passed"]  +=  1
+                metrics["bhk_passed_listings"].append(listing_id)
+            else:
+                metrics["bhk_failed_listings"].append(listing_id)
+
+        ## are check
+        size_found = find_area(desc)
+        if size_found:
+            metrics["size_total"] += 1
+
+            if int(size_found) == int(size):
+                metrics["size_passed"] += 1
+                metrics["size_passed_listings"].append(listing_id)
+            else:
+                metrics["size_failed_listings"].append(listing_id)
+
+        #area.append(find_area(desc))
+        print "AREA ", find_area(desc), ": ", size
+
         ### update metrics
-    print metrics
+    #print metrics
+    for metric in metrics.keys():
+        print metric," : ", metrics[metric] if not metric.endswith("listings") else " "
     print nnn
+    print area
 
 
 
